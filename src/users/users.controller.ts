1f1b463af6aa027824a04b0d6d7a2f3add68e4bf
Fahad Khan <fahad29041994@gmail.com>
2022-04-08 00:14:19 +0500

    File uploads

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,6 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query, UseInterceptors, UploadedFiles } from '@nestjs/common';
+import {FileFieldsInterceptor} from '@nestjs/platform-express'
+
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
@@ -30,5 +32,16 @@ export class UsersController {
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.usersService.remove(id);
+  }
+  
+  @Post('upload')
+  @UseInterceptors(FileFieldsInterceptor([
+    { name: 'avatar', maxCount: 1 },
+    { name: 'background', maxCount: 1 },
+  ], {dest: 'uploads/'} ))
+  uploadFiles(@UploadedFiles() files: { avatar?: Express.Multer.File[], background?: Express.Multer.File[] }) {
+    // create file array with proper contexts then upload in registery against current resource
+    console.log(files.avatar);
+    console.log(files.background);
   }
 }
